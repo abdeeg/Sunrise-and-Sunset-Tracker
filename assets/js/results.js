1d72620ad7d9d379c79948ee5c0e7366a0e26a2b
abdeeg <abdeegdahir@live.com>
2024-01-12 19:46:38 +0000

    Amended results page hero banner and small html and styling fixes

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,28 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
   var searchInputValue = window.localStorage
     .getItem("searchInputValue")
-    .trim().replace(',', '');
-//   console.log(searchInputValue);
+    .trim()
+    .replace(",", "");
+  //   console.log(searchInputValue);
   var options = JSON.parse(window.localStorage.getItem("options"));
-//   console.log(options);
+  //   console.log(options);
   var geoDBURL =
     "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=" +
     searchInputValue +
     "&minPopulation=1000000&limit=10";
-//   console.log(geoDBURL);
+  //   console.log(geoDBURL);
 
-// assign location-title to a variable and append the inputted location
+  // assign location-title to a variable and append the inputted location
   var locationTitle = document.getElementById("location-title");
-  locationTitle.append(searchInputValue)
+  locationTitle.append(searchInputValue);
 
-fetch(geoDBURL, options)
+  fetch(geoDBURL, options)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
 
-    //   console.log(data.data[0]);
+      //   console.log(data.data[0]);
 
       var lat = data.data[0].latitude;
       var lng = data.data[0].longitude;
@@ -47,7 +48,7 @@ fetch(geoDBURL, options)
           document.getElementById("golden-hour").textContent = goldenHour;
           document.getElementById("timezone").textContent = timezone;
 
-        //   locationTitle.textContent = 'hello'
+          //   locationTitle.textContent = 'hello'
         })
         .catch(function (error) {
           console.error("Error fetching sunrise-sunset data:", error);
